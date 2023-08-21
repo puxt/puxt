75e7043ee5601e98ac3f44c182346e76979e7ced
Shabnam Yusifzada <yusifzade.shebnem@gmail.com>
2023-08-21 13:40:42 +0400

    added phpdoc to getMethod method of Request class

--- puxt/Request/Request.php
+++ puxt/Request/Request.php
@@ -6,6 +6,11 @@ namespace Puxt\Request;
 
 class Request implements RequestInterface
 {
+    /**
+     * Get the request method used to access the endpoint
+     *
+     * @return string
+     */
     public function getMethod(): string
     {
         return $_SERVER['REQUEST_METHOD'];
